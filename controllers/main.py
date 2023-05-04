36885e8bb1cef19891d092d8f5587917c0bbeb4c
s4mdaa <ssamdaa119@gmail.com>
2023-05-04 11:30:57 +0800

    to stock picking after login

---
+++ controllers/main.py
@@ -0,0 +1,14 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome
+
+
+class CustomAuthSignupHome(AuthSignupHome):
+    @http.route('/web/login', type='http', auth="none")
+    def web_login(self, redirect=None, **kwargs):
+        response = super(CustomAuthSignupHome, self).web_login(
+            redirect=redirect, **kwargs)
+        if not request.session.uid:
+            return response
+        else:
+            return request.redirect('/web#model=stock.picking')
