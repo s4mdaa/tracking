0bd06ad4064688e41c9c3064227b89d476dce00d
s4mdaa <ssamdaa119@gmail.com>
2023-05-15 17:08:21 +0800

    .

--- model/stock_quant.py
+++ model/stock_quant.py
@@ -22,8 +22,18 @@ class Quant(models.Model):
         'Quantity',
         help='Quantity of products in this quant, in the default unit of measure of the product',
         readonly=True, digits='Product Unit of Measure')
+    reserve_quantity = fields.Float(
+        string='Reserve Quantity', compute='_compute_reserve_quantity', store=True)
     parent_company_id = fields.Many2one(
         'res.company', 'Company', related='company_id.parent_id')
     scheduled_date = fields.Datetime(
         'Scheduled Date', store=True,
         index=True, default=fields.Datetime.now)
+
+    @ api.depends('quantity')
+    def _compute_reserve_quantity(self):
+        for rec in self:
+            if rec.location_id.usage == 'internal':
+                rec.reserve_quantity = rec.quantity
+            else:
+                rec.reserve_quantity = 0
